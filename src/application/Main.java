bc246c88d0ff085a2531f5c39553e4ed357d854b
Torleif Hensvold <torleif.hensvold@outlook.com>
2018-10-16 15:13:51 +0200

    Changed Main.java to start straight to different scene. Will be changed back when #28 is done.

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,6 +1,7 @@
 package application;
 
 import application.database.DB;
+import application.scene.EditorTool;
 import application.scene.Home;
 
 import javafx.application.Application;
@@ -8,49 +9,56 @@ import javafx.stage.Stage;
 
 /**
  * Main.java should run when you start the program.
- * 
+ *
  * @author Niklas Sølvberg
  */
-public class Main extends Application {
-	
-	
+public class Main extends Application
+{
 	@Override
-	public void start(Stage primaryStage) throws Exception {
+	public void start(Stage primaryStage) throws Exception
+	{
 		Stage stage = primaryStage;
 		stage.setMaximized(true);
-
+		
 		Home.showHome(stage, 1, 1);
+		//EditorTool.showEditTool(stage, 1, 1);
+		
+		System.out.println("Out of Main.start");
 	}
 	
-	public static void main(String[] args) {
-		
+	public static void main(String[] args)
+	{
 		try
 		{
 			System.out.println("Connecting to database...");
 			DB.connect();
 			System.out.println("Database connection established.");
-
+			
 			System.out.println("\nApplication running...\n");
 			launch(args);
 			
 			System.out.println("Database connection closing...");
 			DB.disconnect();
 			if (DB.connected())
+			{
 				throw new IllegalStateException("Database connection could not close.");
+			}
 			else
+			{
 				System.out.println("Database connection closed.");
+			}
 		} catch (Exception e)
 		{
 			if (DB.connected())
 			{
 				DB.disconnect();
 				System.out.println("Database connection closed.");
-			} else
+			}
+			else
+			{
 				System.out.println("No connection.");
+			}
 		}
-		
-		
-		
 		
 		
 	}
