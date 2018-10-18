43dd1454c8cded07774d2b3e11f5625da836256b
Torleif Hensvold <torleif.hensvold@outlook.com>
2018-10-18 23:23:17 +0200

    Added TODO messages for the methods connected to #14 and #28

--- src/application/database/Category.java
+++ src/application/database/Category.java
@@ -3,11 +3,14 @@ package application.database;
 public class Category
 {
 
+	//TODO create implementation for #28
 	public static void createCategory(String categoryName)
 	{
 
 	}
 
+
+	//TODO create implementation for #14
 	public static void addCategoryToPost(String categoryName)
 	{
 
