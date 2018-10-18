4cf1cacc60ddcf888ecebc916bbc825718a226e7
Torleif Hensvold <torleif.hensvold@outlook.com>
2018-10-18 23:57:57 +0200

    Added code to createCategory() method. #28
    Added code to create Exceptions when given argument of length 0 or less, or length 100 or more.
    Added try-catch for DB.insert() of query. Probably unnecessary, but better safe than sorry.

--- src/application/database/Category.java
+++ src/application/database/Category.java
@@ -1,22 +1,27 @@
 package application.database;
 
-import java.sql.SQLException;
-
 public class Category
 {
 
 	//TODO create implementation for #28
-	public static void createCategory(String categoryName)
+	public static void createCategory(String categoryName) throws Exception
 	{
 		String query;
-		//if (categoryName.length()>=1 && categoryName.length()<=100)
+		if (categoryName.length() < 1)
+		{
+			throw new IllegalArgumentException("Category name must be at least 1 character long.");
+		} else if (categoryName.length() > 100)
+		{
+			throw new IllegalArgumentException("Category name must be less than 100 characters long.");
+		}
 		try
 		{
 			query = "INSERT INTO categories (categoryName) VALUE (" + categoryName + ");";
 			DB.insert(query);
-		} catch (SQLException se)
+		} catch (Exception e)
 		{
-
+			throw new Exception("CreateCategory in Category in database");
+			// TODO Add better Exception message. Slightly dependant on DB.java methods.
 		}
 	}
 
