a63759f39fdb4f0352fd744a6e327b9740388afe
Torleif Hensvold <torleif.hensvold@outlook.com>
2018-11-05 14:57:22 +0100

    Refactored getLabels() to remove duplicated code

--- src/application/logic/Posts.java
+++ src/application/logic/Posts.java
@@ -170,19 +170,9 @@ public class Posts {
 	 */
 	public static ArrayList<Label> getLabels() {
 		ArrayList<Label> labels = new ArrayList<Label>();
-		for (Post post : getPosts()) {
-			String[] wordList = post.getBody().split(" ");
-			String body = "";
-			int i = 0;
-			for (int j = 0; j < wordList.length; j++) {
-				if (i > 100) {
-					body += "\n";
-					i = 0;
-				}
-				i += wordList[j].length() + 1;
-				body += wordList[j] + " ";
-			}
-			labels.add(new Label("____________________________________________________________________________________________________\n____________________________________________________________________________________________________\n\n\n" + post.getHeader() + ", by " + post.getPoster() + "\n--------------------------------------------------\n" + body));
+		for (Post post : getPosts())
+		{
+			labels.add(getPostLabel(post));
 		}
 		return labels;
 	}
